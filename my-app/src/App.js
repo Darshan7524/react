3413c802d97690007c79ba8ee4c5907b0cba308b
Darshan V Simson <75269582+Darshan7524@users.noreply.github.com>
2022-07-07 17:46:43 +0530

    app.js (use effect , usestate)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
 import { Helloworld, Byeworld } from './components/Helloworld';
+import {useState, useEffect } from 'react'
 
 function App() {
+  const [counter, setcounter] = useState(0);
+const increment = ()=>
+{
+  setcounter(counter+1);
+}
+console.log(counter);
+
+useEffect(() => {
+  document.title = `clicked ${counter} times `
+
+ 
+},[counter])
+
+
+
   return (
     <div className="App">
       <h1>first line</h1>
       <p> I am learning react.js</p>
      <Helloworld text="react js is a javascript library"></Helloworld>
      <Byeworld text = "reactjs is easy"></Byeworld>
+     <button onClick={increment}>click here!</button>
+     <h1>{counter}</h1>
     </div>
   );
 }
